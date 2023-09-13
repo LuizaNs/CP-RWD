d851afa6ddb752a0a6db3b1239b29733957322d4
LuizaNs <luizansjs@gmail.com>
2023-09-13 09:24:06 -0300

    Iniciando a configuração das rotas (path e elements) com o createBrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>, 
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path:"/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+  }
+])
+
+//errorElement - mostra o componente a ser apresentado caso a URL não exista.
+//children - atributo de App que vai armazenar um array (coleção de obejtos, como uma lista) os objetos de todas as páginas que podem ser acessadas. 
+//path - define a URI da página após o endereço do projeto no browser.
+//element - define o componente que vai ser chamado quando a URI for chamada no browser 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
